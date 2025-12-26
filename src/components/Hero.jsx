450aeb8bd525c460c5e2995af3868bfd4f0c28e1
nadeem35 <nadeem.cse72@gmail.com>
2025-12-27 00:26:12 +0530

    update design

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,8 @@
 import { assets } from '../assets/assets'
-
 const Hero = () => {
   return (
-    <div className='w-full py-40'>
-      <div className="container w-full px-10">
+    <div className='w-full bg-primary/10 py-40'>
+      <div className="container mx-auto w-full px-10">
         <div className="w-75 py-1 text-dark/80 border border-primary rounded-full flex justify-center items-center mx-auto text-center">
           <div className="flex items-center -space-x-3">
             <img src={assets.profile1Img} className='h-8 w-8 rounded-full flex justify-center text-center object-center object-cover' alt="profile1" />
@@ -14,12 +13,15 @@ const Hero = () => {
           <span className='ml-3'>Trusted by 20k+ users</span>
         </div>
 
-        <h1 className='text-center py-5 text-7xl'>We Help Brands Grow With <span className='text-primary'> Performance </span> Marketing</h1>
+        <h1 className='text-center py-5 text-4xl sm:text-5xl md:text-6xl lg:text-7xl'>We Help Brands Grow With <span className='text-primary'> Performance </span> Marketing</h1>
         <p className='max-w-xl font-thin mx-auto text-2xl text-dark/70 text-center'>At our agency, we take pride in delivering impactful solutions that combine creativity, strategy, and technology.</p>
-
         <div className="py-10 space-x-10 flex justify-center items-center font-semibold">
-          <button className='px-10 py-3 bg-primary cursor-pointer rounded-full'>Contact Me</button>
-          <button className='px-10 py-3 bg-dark rounded-full cursor-pointer text-amber-50'>Digital Market</button>
+          <button className='px-10 py-3 bg-primary hover:bg-primary/90 cursor-pointer rounded-full'>Contact Me</button>
+          <button className='px-10 py-3 bg-dark hover:bg-dark/90 rounded-full cursor-pointer text-amber-50'>Digital Market</button>
+        </div>
+
+        <div className="max-w-5xl flex mx-auto justify-center items-center overflow-hidden">
+          <img className='rounded-xl object-cover' src={assets.heroImg} alt="hero-image" />
         </div>
       </div>
     </div>
