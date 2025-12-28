0162ac7d55e5a798451ebc7250221fbdde8779f3
nadeem35 <nadeem.cse72@gmail.com>
2025-12-29 00:13:05 +0530

    add

--- src/main.jsx
+++ src/main.jsx
@@ -1,31 +1,30 @@
-// import { StrictMode } from 'react'
-// import { createRoot } from 'react-dom/client'
-// import './index.css'
-// import App from './App.jsx'
-// import { BrowserRouter, HashRouter } from 'react-router-dom'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { BrowserRouter, HashRouter } from 'react-router-dom'
 
-// createRoot(document.getElementById('root')).render(
-//   <StrictMode>
-//     <HashRouter>
-
-//       <BrowserRouter>
-//         <App />
-//       </BrowserRouter >
-//     </HashRouter>
-//   </StrictMode>
-// )
-
-
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.jsx";
-import { HashRouter } from "react-router-dom";
-
-createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById('root')).render(
   <StrictMode>
     <HashRouter>
+      {/* <BrowserRouter> */}
       <App />
+      {/* </BrowserRouter > */}
     </HashRouter>
   </StrictMode>
-);
+)
+
+
+// import { StrictMode } from "react";
+// import { createRoot } from "react-dom/client";
+// import "./index.css";
+// import App from "./App.jsx";
+// import { HashRouter } from "react-router-dom";
+
+// createRoot(document.getElementById("root")).render(
+//   <StrictMode>
+//     <HashRouter>
+//       <App />
+//     </HashRouter>
+//   </StrictMode>
+// );
